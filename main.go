31d646a4fba1809309f3037b8d52bb739582e360
Sergey Bronnikov <sergeyb@openvz.org>
2016-02-04 20:32:29 +0300

    Add template, style file and source code

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"gopkg.in/yaml.v2"
+)
+
+type Game struct {
+	Name		string
+	Info		string
+	URL			string
+	Screencast	string
+	Play		string
+}
+
+func Read() ([]string) {
+	a := []string{"tetris", "orange", "pear"}
+	return a
+}
+
+func main() {
+
+	var data = "games.yaml"
+	var html = "template.html"
+
+	if _, err := os.Stat(data); os.IsNotExist(err) {
+		fmt.Printf("File %v doesn't exist.\n", data)
+		os.Exit(1)
+	}
+
+	f, _ := filepath.Abs(data)
+	yamlFile, err := ioutil.ReadFile(f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	games := []Game{}
+	type GameList []Game
+
+	err = yaml.Unmarshal(yamlFile, &games)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	f, _ = filepath.Abs(html)
+	htmltmpl, err := ioutil.ReadFile(html)
+
+	if err != nil {
+		panic(err)
+	}
+
+	t := template.Must(template.New("tmpl").Parse(string(htmltmpl)))
+	t.Execute(os.Stdout, games)
+}
