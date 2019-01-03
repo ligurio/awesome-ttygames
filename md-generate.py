6c62b9c0bffb3eedfa26594022bc9729d6de7d16
Oskar Skog <oskar.skog.finland@gmail.com>
2019-01-03 23:58:26 +0200

    I think it would be better to have the screencast links in the body instead of the header

--- md-generate.py
+++ md-generate.py
@@ -9,24 +9,28 @@ sys.setdefaultencoding('utf-8')
 stream = open("games.yaml", "r")
 ttygames = yaml.load(stream)
 for entry in ttygames:
-
+    
     if 'url' in entry:
         header = "### [%s](%s)" % (entry['name'], entry['url'])
     else:
         header = "### %s" % entry['name']
-
-    if 'screencast' in entry:
-        header = header + " [Screencast](%s)" % entry['screencast']
-
+    
+    # It looks like "Screencast" is part of the title
+    #if 'screencast' in entry:
+        #header = header + " [Screencast](%s)" % entry['screencast']
+    
     print header, "\n"
-
+    
     if 'info' in entry:
         print("%s\n" % entry['info'])
-
+    
+    if 'screencast' in entry:
+        print("Watch [screencast](%s).\n" % entry['screencast'])
+    
     if 'wikipedia' in entry:
         print("See also [Wikipedia](%s).\n" % entry['wikipedia'])
-
+    
     if 'play' in entry:
         print("\n**Play**: ```%s```\n" % entry['play'])
-
+    
     print
